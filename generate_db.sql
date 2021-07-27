e1290c81c39840150b4bddc394de2678430c9eb1
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-07-27 17:58:26 -0500

    add 'retos' to table

--- generate_db.sql
+++ generate_db.sql
@@ -1,49 +1,50 @@
 CREATE DATABASE exviolines;
 USE exviolines;
 
-CREATE TABLE `Retos` (
-  `reto_id` int auto_increment,
-  `nombre` varchar(50),
-  `categoría` varchar(20),
-  `descripcion` varchar(300),
-  `puntos` int,
-  `grado_id` int,
-  PRIMARY KEY (`reto_id`),
-  FOREIGN KEY (`grado_id`) REFERENCES `Grados`(`grado_id`)
-);
-
-CREATE TABLE `Grados` (
+CREATE TABLE `grados` (
   `grado_id` int auto_increment,
   `anno` int,
   `grado` int,
   PRIMARY KEY (`grado_id`)
 );
 
-CREATE TABLE `Alumno` (
-  `alumno_id` int auto_increment,
+CREATE TABLE `retos` (
+  `reto_id` int auto_increment,
+  `nombre` varchar(100),
+  `categoria` varchar(20),
+  `descripcion` varchar(300),
+  `puntos` int,
+  `grado_id` int,
+  PRIMARY KEY (`reto_id`),
+  FOREIGN KEY (`grado_id`) REFERENCES `grados`(`grado_id`)
+);
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int auto_increment,
   `nombre` varchar(20),
-  `apellidos` varchar(50),
+  `apellido` varchar(50),
   `correo` varchar(50),
   `password` varchar(100),
   `puntosAcumulados` int DEFAULT 0,
   `seguimiento` varchar(100) DEFAULT 'TBA',
   `asistenciaEventos` boolean DEFAULT False,
   `grado_id` int,
-  PRIMARY KEY (`alumno_id`),
-  FOREIGN KEY (`grado_id`) REFERENCES `Grados`(`grado_id`)
+  `rol` varchar(100) DEFAULT 'Alumno',
+  PRIMARY KEY (`usuario_id`),
+  FOREIGN KEY (`grado_id`) REFERENCES `grados`(`grado_id`)
 );
 
-CREATE TABLE `Logros` (
-  `alumno_id` int,
+CREATE TABLE `logros` (
+  `usuario_id` int,
   `reto_id` int,
   `archivo` varbinary(32),
   `fecha` timeStamp,
-  PRIMARY KEY (`alumno_id`, `reto_id`),
-  FOREIGN KEY (`alumno_id`) REFERENCES `Alumno`(`alumno_id`),
-  FOREIGN KEY (`reto_id`) REFERENCES `Retos`(`reto_id`)
+  PRIMARY KEY (`usuario_id`, `reto_id`),
+  FOREIGN KEY (`usuario_id`) REFERENCES `usuarios`(`usuario_id`),
+  FOREIGN KEY (`reto_id`) REFERENCES `retos`(`reto_id`)
 );
 
-CREATE TABLE `Eventos` (
+CREATE TABLE `eventos` (
   `evento_id` int auto_increment,
   `lugar` varchar(200),
   `fecha` datetime,
@@ -60,3 +61,49 @@ VALUES(2021, 2);
 INSERT INTO grados(anno, grado)
 VALUES(2021, 3);
 
+INSERT INTO grados(anno, grado)
+VALUES(2021, 4);
+
+INSERT INTO grados(anno, grado)
+VALUES(2021, 5);
+
+INSERT INTO grados(anno, grado)
+VALUES(2021, 6);
+
+INSERT INTO grados(anno, grado)
+VALUES(2021, 7);
+
+
+INSERT INTO Usuarios(nombre, apellido, correo, password, grado_id, rol)
+VALUES ('admin', 'admin', 'admin@admin.com', 'admin', 7, 'admin');
+
+
+INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+VALUES ('Reto 1: Reading and vocabulary', 'Ingles', 'Read carefully what the monster says and answer the questions below. You can write the answers or upload a picture of them.', 10, 1);
+
+INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+VALUES ('Reto 2: Present Simple - Negative and Positive Sentences', 'Ingles', 'Fill the blanks with the correct form of present simple of each verb in parentheses, some of them are positive and others are negative. You can write the answers or upload a picture of them.', 10, 4);
+
+INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+VALUES ('Reto 3: La jungle et ses animaux', 'Frances', "Associez le mot à l'image correspondante.", 10, 1);
+
+INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+VALUES ('Reto 4: Verbe être et avoir', 'Frances', 'Résoudre les éléments suivants être.', 10, 7);
+
+INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+VALUES ('Reto 5: Meistermaler', 'Aleman', 'Dibuja cada objeto con su color correspondiente.', 10, 7);
+
+-- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+-- VALUES ('', '', '', 10, 1);
+
+-- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+-- VALUES ('', '', '', 10, 1);
+
+-- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+-- VALUES ('', '', '', 10, 1);
+
+-- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+-- VALUES ('', '', '', 10, 1);
+
+-- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
+-- VALUES ('', '', '', 10, 1);
