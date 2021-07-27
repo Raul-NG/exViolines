59504aa6e9891df78f25f2b0fe63e252d1194699
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-07-27 02:47:57 -0500

    fix connection to db and add fields for sign up. TODO: fix 'grado'

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -1,15 +1,17 @@
 <?php
-
+    session_start();
     include 'conexion_be.php';
 
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
 
-    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo'
-                    and contrasena= '$contrasena' "); 
+
+    $validar_login = mysqli_query($conexion, "SELECT * FROM alumno WHERE correo='$correo'
+                    and password= '$contrasena' "); 
 
     if(mysqli_num_rows($validar_login) >0){
-        header("location: ../bienvenida.html");
+        $_SESSION['loggedin'] = true;
+        header("location: ../inicio.php");
         exit;
     }else{
         echo '
--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -2,19 +2,20 @@
 
     include 'conexion_be.php';
 
-    $nombre_completo = $_POST['nombre_completo'];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
-    $grado = $_POST['grado'];
+    $grado_id = $_POST['grado_id'];
     //Si se quisiera encriptar la contrasena esta la siguiente linea de codigo
     //$contrasena = hash('sha512', $contrasena);
 
-    $query = "INSERT INTO usuarios(nombre_completo, correo, contrasena, grado)
-              VALUES('$nombre_completo','$correo', '$contrasena', '$grado')";
+    $query = "INSERT INTO alumno(nombre, apellidos, correo, password, grado_id)
+    VALUES ('$nombre', '$apellido', '$correo', '$contrasena', '$grado_id')";
 
     //Verficiar que el correo no se repita en la Base de Datos
 
-    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' ");
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM alumno WHERE correo='$correo' ");
 
     if(mysqli_num_rows($verificar_correo) >0){
         echo'
