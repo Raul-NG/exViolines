29195db7ca0db1dd6a1eb9832f45ccea67b2e3ba
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-07-28 21:31:03 -0500

    show completed/to deliver activities

--- misRetos.php
+++ misRetos.php
@@ -12,7 +12,7 @@ include './php/conexion_be.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-  <link rel="stylesheet" href="assets/css/estilos.css" />
+  <link rel="stylesheet" href="./assets/css/estilos.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <!-- Link fuentes https://fonts.google.com -->
@@ -26,10 +26,14 @@ include './php/conexion_be.php';
   <h1 class="h1-pags">Mis Retos</h1>
     <div class="container mt-5 mb-5">
       <div class="row justify-content-center">
+        <h3>Por entregar</h3>
     <?php
       $retos = mysqli_query($conexion, "SELECT retos.nombre, categoria, puntos, descripcion FROM retos
       JOIN usuarios ON retos.grado_id = usuarios.grado_id
-      WHERE usuarios.correo = 'admin@admin.com'");
+      WHERE usuarios.correo = '$_SESSION[user]' AND retos.reto_id
+      NOT IN
+      (SELECT logros.reto_id FROM logros
+      WHERE usuarios.correo = '$_SESSION[user]')");
       while( $row = mysqli_fetch_assoc($retos)) {
         echo "
         <div class='col-12 col-sm-6 col-md-4 col-lg-4 mt-3'>
@@ -42,7 +46,7 @@ include './php/conexion_be.php';
                 $row['categoria'] .
               " </p>
               <p class='card-points'> " . 
-                $row['puntos'] . " Puntos
+                "-/" . $row['puntos'] . " Puntos
               </p>
               <p class='card-text'> " .
                 $row['descripcion'] .
@@ -57,6 +61,31 @@ include './php/conexion_be.php';
                 <input type='file' id='myFile' name='filename'>
                 <input type='submit'>
               </form>
+            </div>
+          </div>
+        </div>";} 
+    ?>
+    <h3>Entregados</h3>
+    <?php
+      $retos = mysqli_query($conexion, "SELECT retos.nombre, categoria, puntos, descripcion FROM retos
+      JOIN usuarios ON retos.grado_id = usuarios.grado_id
+      JOIN logros ON logros.reto_id = retos.reto_id
+      WHERE usuarios.correo = '$_SESSION[user]' AND retos.reto_id");
+      while( $row = mysqli_fetch_assoc($retos)) {
+        echo "
+        <div class='col-12 col-sm-6 col-md-4 col-lg-4 mt-3'>
+          <div class='card'>
+            <img src='./imagenes/retos_imagenes/reto1.jpg' class='card-img-top'
+              alt='Palomitas, boletos para el cine y rollo de película' />
+            <div class='card-body'>
+              <h2 class='card-title'>" . $row['nombre'] . " </h2>
+              <p class='card-grado'> " .
+                $row['categoria'] .
+              " </p>
+              <p class='card-points' > " . 
+              $row['puntos'] . "/" . $row['puntos'] . " Puntos
+              </p>
+              <p style='text-align:center; font-size:30px;'> <span>&#9989;</span></p>
             </div>
           </div>
         </div>";} 
