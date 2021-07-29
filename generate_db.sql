98f864d3283b816faa9becbfc1690b285da3011a
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-07-28 22:20:03 -0500

    remove 'puntos acumulados' and add 'image_id'

--- generate_db.sql
+++ generate_db.sql
@@ -15,6 +15,7 @@ CREATE TABLE `retos` (
   `descripcion` varchar(300),
   `puntos` int,
   `grado_id` int,
+  `image_id` int,
   PRIMARY KEY (`reto_id`),
   FOREIGN KEY (`grado_id`) REFERENCES `grados`(`grado_id`)
 );
@@ -25,7 +26,6 @@ CREATE TABLE `usuarios` (
   `apellido` varchar(50),
   `correo` varchar(50),
   `password` varchar(100),
-  `puntosAcumulados` int DEFAULT 0,
   `seguimiento` varchar(100) DEFAULT 'TBA',
   `asistenciaEventos` boolean DEFAULT False,
   `grado_id` int,
@@ -78,20 +78,30 @@ INSERT INTO usuarios(nombre, apellido, correo, password, grado_id, rol)
 VALUES ('admin', 'admin', 'admin@admin.com', 'admin', 7, 'admin');
 
 
-INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id)
-VALUES ('Reto 1: Reading and vocabulary', 'Ingles', 'Read carefully what the monster says and answer the questions below. You can write the answers or upload a picture of them.', 10, 1);
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 1: Reading and vocabulary', 'Ingles', 'Read carefully what the monster says and answer the questions below. You can write the answers or upload a picture of them.', 10, 1, 1);
 
-INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id)
-VALUES ('Reto 2: Present Simple - Negative and Positive Sentences', 'Ingles', 'Fill the blanks with the correct form of present simple of each verb in parentheses, some of them are positive and others are negative. You can write the answers or upload a picture of them.', 10, 4);
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 1: Reading and vocabulary', 'Ingles', 'Read carefully what the monster says and answer the questions below. You can write the answers or upload a picture of them.', 10, 7, 1);
 
-INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id)
-VALUES ('Reto 3: La jungle et ses animaux', 'Frances', "Associez le mot à l'image correspondante.", 10, 1);
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 2: Present Simple - Negative and Positive Sentences', 'Ingles', 'Fill the blanks with the correct form of present simple of each verb in parentheses, some of them are positive and others are negative. You can write the answers or upload a picture of them.', 10, 4, 2);
 
-INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id)
-VALUES ('Reto 4: Verbe être et avoir', 'Frances', 'Résoudre les éléments suivants être.', 10, 7);
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 2: Present Simple - Negative and Positive Sentences', 'Ingles', 'Fill the blanks with the correct form of present simple of each verb in parentheses, some of them are positive and others are negative. You can write the answers or upload a picture of them.', 10, 7, 2);
 
-INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id)
-VALUES ('Reto 5: Meistermaler', 'Aleman', 'Dibuja cada objeto con su color correspondiente.', 10, 7);
+
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 3: La jungle et ses animaux', 'Frances', "Associez le mot à l'image correspondante.", 10, 1, 3);
+
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 3: La jungle et ses animaux', 'Frances', "Associez le mot à l'image correspondante.", 10, 7, 3);
+
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 4: Verbe être et avoir', 'Frances', 'Résoudre les éléments suivants être.', 10, 7, 4);
+
+INSERT INTO retos(nombre, categoria, descripcion, puntos, grado_id, image_id)
+VALUES ('Reto 5: Meistermaler', 'Aleman', 'Dibuja cada objeto con su color correspondiente.', 10, 7, 5);
 
 -- INSERT INTO Retos(nombre, categoria, descripcion, puntos, grado_id)
 -- VALUES ('', '', '', 10, 1);
@@ -111,3 +121,6 @@ VALUES ('Reto 5: Meistermaler', 'Aleman', 'Dibuja cada objeto con su color corre
 
 INSERT INTO logros(usuario_id, reto_id, archivo, fecha)
 VALUES (1, 4, "ASDASD", NOW());
+
+INSERT INTO logros(usuario_id, reto_id, archivo, fecha)
+VALUES (1, 5, "ASDASD", NOW());
