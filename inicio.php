dd0234d0ab4f43ad5d306ab7b01d91906c829094
Manuel Hernandez <48745085+manuelhb14@users.noreply.github.com>
2021-07-29 01:50:35 -0500

    add css styles

--- inicio.php
+++ inicio.php
@@ -16,7 +16,7 @@ session_start();
     pierdas el contacto con tus amigos. En Violines por la Paz A.C. buscamos que todos los niños tengan una educación integral y balanceada." />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-  <link rel="stylesheet" href="/assets/css/estilos.css" />
+  <link rel="stylesheet" href="./assets/css/estilos.css" />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <!-- Link fuentes https://fonts.google.com -->
